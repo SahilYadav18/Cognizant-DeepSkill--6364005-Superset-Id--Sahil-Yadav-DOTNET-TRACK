f0581ffe6f367a263f9147af28f8ef12de920e33
SahilYadav18 <145473592+SahilYadav18@users.noreply.github.com>
2025-07-06 22:52:11 +0530

    Add files via upload

---
+++ Week-1/Exercise-2/Exercise-2.cs
@@ -0,0 +1,58 @@
+﻿//Abstract Document class
+public abstract class Document
+{
+    public abstract void PrintType();
+}
+
+//Concrete Document classes
+public class WordDocument : Document
+{
+    public override void PrintType() => Console.WriteLine("This is a Word document.");
+}
+public class PdfDocument : Document
+{
+    public override void PrintType() => Console.WriteLine("This is a PDF document.");
+}
+public class ExcelDocument : Document
+{
+    public override void PrintType() => Console.WriteLine("This is an Excel document.");
+}
+
+//Abstract Factory
+public abstract class DocumentFactory
+{
+    public abstract Document CreateDocument();
+}
+
+//Concrete Factories
+public class WordDocumentFactory : DocumentFactory
+{
+    public override Document CreateDocument() => new WordDocument();
+}
+public class PdfDocumentFactory : DocumentFactory
+{
+    public override Document CreateDocument() => new PdfDocument();
+}
+public class ExcelDocumentFactory : DocumentFactory
+{
+    public override Document CreateDocument() => new ExcelDocument();
+}
+
+//Test Factory Method Pattern
+class Program
+{
+    static void Main(string[] args)
+    {
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        Document wordDoc = wordFactory.CreateDocument();
+        wordDoc.PrintType();
+
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Document pdfDoc = pdfFactory.CreateDocument();
+        pdfDoc.PrintType();
+
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        Document excelDoc = excelFactory.CreateDocument();
+        excelDoc.PrintType();
+    }
+}
